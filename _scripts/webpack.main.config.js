020a7ef2b3aa4df0acc721f30958ee0a82f488b3
Shugen <shugen002@users.noreply.github.com>
2020-05-26 23:38:46 +0800

    Bump copy-webpack-plugin from 5.1.1 to 6.0.1

--- _scripts/webpack.main.config.js
+++ _scripts/webpack.main.config.js
@@ -3,9 +3,9 @@ const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const {
-  dependencies,
-  devDependencies,
-  productName,
+    dependencies,
+    devDependencies,
+    productName,
 } = require('../package.json')
 
 const externals = Object.keys(dependencies).concat(Object.keys(devDependencies))
@@ -13,73 +13,74 @@ const isDevMode = process.env.NODE_ENV === 'development'
 const whiteListedModules = []
 
 const config = {
-  name: 'main',
-  mode: process.env.NODE_ENV,
-  devtool: isDevMode ? '#cheap-module-eval-source-map' : false,
-  entry: {
-    main: path.join(__dirname, '../src/main/index.js'),
-  },
-  externals: externals.filter((d) => !whiteListedModules.includes(d)),
-  module: {
-    rules: [
-      {
-        test: /\.(j|t)s$/,
-        loader: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.node$/,
-        use: 'node-loader',
-      },
-    ],
-  },
-  node: {
-    __dirname: isDevMode,
-    __filename: isDevMode,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.PRODUCT_NAME': JSON.stringify(productName),
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-    path: path.join(__dirname, '../dist'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    alias: {
-      '@': path.join(__dirname, '../src/'),
-      src: path.join(__dirname, '../src/'),
+    name: 'main',
+    mode: process.env.NODE_ENV,
+    devtool: isDevMode ? '#cheap-module-eval-source-map' : false,
+    entry: {
+        main: path.join(__dirname, '../src/main/index.js'),
     },
-  },
-  target: 'electron-main',
+    externals: externals.filter((d) => !whiteListedModules.includes(d)),
+    module: {
+        rules: [
+            {
+                test: /\.(j|t)s$/,
+                loader: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.node$/,
+                use: 'node-loader',
+            },
+        ],
+    },
+    node: {
+        __dirname: isDevMode,
+        __filename: isDevMode,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.PRODUCT_NAME': JSON.stringify(productName),
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        libraryTarget: 'commonjs2',
+        path: path.join(__dirname, '../dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            '@': path.join(__dirname, '../src/'),
+            src: path.join(__dirname, '../src/'),
+        },
+    },
+    target: 'electron-main',
 }
 
 if (isDevMode) {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      __static: `"${path.join(__dirname, '../static').replace(/\\/g, '\\\\')}"`,
-    })
-  )
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            __static: `"${path.join(__dirname, '../static').replace(/\\/g, '\\\\')}"`,
+        })
+    )
 } else {
-  config.plugins.push(
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, '../src/data'),
-        to: path.join(__dirname, '../dist/data'),
-      },
-      {
-        from: path.join(__dirname, '../static'),
-        to: path.join(__dirname, '../dist/static'),
-        ignore: ['.*'],
-      },
-    ]),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    })
-  )
+    config.plugins.push(
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, '../src/data'),
+                    to: path.join(__dirname, '../dist/data'),
+                },
+                {
+                    from: path.join(__dirname, '../static'),
+                    to: path.join(__dirname, '../dist/static'),
+                },
+            ]
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+        })
+    )
 }
 
 module.exports = config
