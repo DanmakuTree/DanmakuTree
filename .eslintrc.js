3ac5b9b55db3d49e6d09d07b74cee50d20bc1ad6
mano233 <2360861135@qq.com>
2020-05-28 02:02:44 +0800

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,18 @@ module.exports = {
     'vue'
   ],
   rules: {
-  }
+    // 缩进两空格（switch case两空格）
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2, { baseIndent: 1 }]
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        // 解决vue缩进规则冲突
+        indent: 'off'
+      }
+    }
+  ]
 }
