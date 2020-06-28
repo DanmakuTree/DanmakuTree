f3b209633062fc79718d792205d30becdd3bae7b
ax4 <ax4@catcode.xyz>
2020-06-28 01:01:37 +0100

    Optimize the userlittle clicking area to larger

--- src/renderer/components/menu/userlittle.vue
+++ src/renderer/components/menu/userlittle.vue
@@ -1,10 +1,10 @@
 <!--显示用户头像和用户名的UI组件-->
 <template>
-  <div style="display: flex;align-items: center">
+  <div style="display: flex;align-items: center" @click="login">
     <div style="width: 32px;height: 32px;border-radius: 50%;">
       <img :src="userInfo.face" width="100%" height="100%"/>
     </div>
-    <div style="margin-left: 8px;display: flex;flex-direction: column;justify-content: space-between" @click="login">
+    <div style="margin-left: 8px;display: flex;flex-direction: column;justify-content: space-between">
       <div style="font-size: 12px;font-weight: bold;color: #171725">{{userInfo.name}}</div>
       <div style="font-size: 12px;">哔哩哔哩</div>
     </div>
